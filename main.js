b3bc80c42d8e5b67e55bf2d05d0eb614fbae4483
Aster wang <98342555+AAAAster@users.noreply.github.com>
2022-05-08 13:15:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,40 @@
-lst = [];
-ac = 0;
-sta = 1;
-t = 0;
-playerY = 0;
+locY = [];
+playY = 680;
+
 function setup() {
-	createCanvas(300, 800);
+	createCanvas(300, 700);
 	background(250);
-	for(let n = 40; n <= 700; n = n + random(60,100)){
-		append(lst,n);
-		ac = ac + 1;
-	}
-	for(let i=0; i<= ac; i = i+1) {
-		platform(150,lst[i]);
-	}
+	creatMap();	//生成地图,locY储存了每个地图点上的Y值
+	print('setup finish');
+	print(locY);
 }
 
-function platform(x,y){
-	wide = random(300,400)/2;
-	hight = random(20,30)/2;
+
+function creatMap(){
+	//生成随机Y地址
+	for(let poss = 650; poss >= 0 ; poss -= random(45,60) ){
+		append(locY, poss);	//生成坐标点，储存在locY中
+		drawPlatform(random(100,200),poss);//绘制地图
+		}
+	textSize(10);
+	text('you start here➡️', 30, 690);
+		
+}
+function drawPlatform(x,y){
+	wide = random(200,300)/2;
+	hight = random(15,20)/2;
 	fill(random(100,255),random(0,200),random(100,255));
 	noStroke();
 	quad(x-wide,y+hight,x+wide,y+hight,x+wide,y-hight,x-wide,y-hight);
-	
 }
 
+
+function draw(){
+	fill(255, 204, 0);
+	game();
+}
 function game(){
-	while(sta == 1){
-		while(keyIsPressed === true){
-			t = t + 1;
-			sleep(600);
-			role(playerY,t);
-			if (t > 30){
-				break;
-			}
-		}
-		playerY = playerY +t;
-		role(playerY,0);
-		for(let i=0; i<= ac; i = i+1) {
-			if(playerY != i){
-				sta = 0;
-			}
-	}
-		
-	}
+	circle(150, playY, 25);	//x,y,w 显示角色位置
 }
 
-function role(playerY,t){
-	fill(0);
-	rect(150,playerY,10,35-t);
-}
 
-game();
